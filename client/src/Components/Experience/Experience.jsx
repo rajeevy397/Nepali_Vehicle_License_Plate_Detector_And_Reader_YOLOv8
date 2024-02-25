44123b5b43be02481b2db928b48eeac8885557e8
Rajkishor Yadav <076bct065@ioepc.edu.np>
2024-02-25 23:43:58 +0545

    Rajkishor-FrontendUpdated

---
+++ client/src/Components/Experience/Experience.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Experience.css';
+
+const Experience = () => {
+  return (
+    <div className="experience" id='Experience'>
+      <div className="achievement">
+        <div className="circle">1+</div>
+        <span>years</span>
+        <span>Experience</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">5+</div>
+        <span>completed </span>
+        <span>Projects</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">2+</div>
+        <span>companies </span>
+        <span>Work</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Toggle from '../Toggle/Toggle';
+import './Navbar.css';
+import { Link } from 'react-scroll'; 
+import Logo from '../../img/Logo.png';
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name"><img src={Logo} alt='1.png'/></div>
+        <Toggle />
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: 'none' }}>
+            <Link
+              spy={true}
+              to="Navbar"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li style={{ cursor: 'pointer', color: '#788097' }} onMouseEnter={(e) => e.target.style.color = 'rgb(151, 16, 235)'} onMouseLeave={(e) => e.target.style.color = '#788097'}>Home</li>
+            </Link>
+            <Link spy={true} to="Services" smooth={true}>
+              <li style={{ cursor: 'pointer', color: '#788097' }} onMouseEnter={(e) => e.target.style.color = 'rgb(151, 16, 235)'} onMouseLeave={(e) => e.target.style.color = '#788097'}>Features</li>
+            </Link>
+            {/* <Link spy={true} to="Experience" smooth={true}>
+              <li>Experience</li>
+            </Link> */}
+            <Link spy={true} to="Portfolio" smooth={true}>
+              <li style={{ cursor: 'pointer', color: '#788097' }} onMouseEnter={(e) => e.target.style.color = 'rgb(151, 16, 235)'} onMouseLeave={(e) => e.target.style.color = '#788097'}>SomeOutputs</li>
+            </Link>
+            <Link spy={true} to="Testimonials" smooth={true}>
+              <li style={{ cursor: 'pointer', color: '#788097' }} onMouseEnter={(e) => e.target.style.color = 'rgb(151, 16, 235)'} onMouseLeave={(e) => e.target.style.color = '#788097'}>Team</li>
+            </Link>
+          </ul>
+        </div>
+        <Link spy={true} to="Contact" smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
